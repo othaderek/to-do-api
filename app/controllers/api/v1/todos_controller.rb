14030201f33b3fea6b54e153d58705e9754ab498
othaderek <otha.hernandez@gmail.com>
2020-02-13 21:35:14 -0500

    adds code

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -5,22 +5,24 @@ class Api::V1::TodosController < ApplicationController
         render json: @todos
     end
 
-    
+
 
     def create
         @todo = Todo.create(todo_params)
 
-        if @todo.valid?
-          render json: @todo
-        else
-          render json: {errors: todo.errors.full_messages}, status: unprocessable_entity
-        end
+        render json: @todo
+
+        # if @todo.valid?
+        #   render json: @todo
+        # else
+        #   render json: {errors: @todo.errors.full_messages}, status: unprocessable_entity
+        # end
     end
 
     private
 
     def todo_params
-        params.permit(:body, :completed, :user_id)
+        params.require(:todo).permit(:body, :completed, :user_id)
     end
 
 end
