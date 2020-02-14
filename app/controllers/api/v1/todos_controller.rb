3035bd71a4f97acb64ae2bc3060b728ccf1b12fd
othaderek <otha.hernandez@gmail.com>
2020-02-14 12:34:04 -0500

    Update action in Todo controller added.

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -5,6 +5,23 @@ class Api::V1::TodosController < ApplicationController
         render json: @todos
     end
 
+    def show    
+        @todo = Todo.find(todo_params)
+        render json: @todo
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+
+        if @todo
+            @todo.update(todo_params)
+            render json: @todo
+        else
+            render json: {errors: @todo.errors.full_messages}, status: unprocessable_entity
+        end
+
+    end
+
 
 
     def create
